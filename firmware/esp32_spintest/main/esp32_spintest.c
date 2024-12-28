76aa344a80eee0e9080d9c613b3257a8167fa881
Joseph <jgoyer3@gmail.com>
2024-12-27 23:21:43 -0500

    spin test firmware

---
+++ firmware/esp32_spintest/main/esp32_spintest.c
@@ -0,0 +1,76 @@
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+#include "esp_err.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "hal/gpio_types.h"
+#include <stdio.h>
+
+#define LEDC_TIMER LEDC_TIMER_0
+#define LEDC_MODE LEDC_LOW_SPEED_MODE
+#define LEDC_CHANNEL LEDC_CHANNEL_0
+#define LEDC_DUTY_RES LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY (4096)      // Set duty to 50%. (2 ** 13) * 50% = 4096
+#define LEDC_FREQUENCY (4000) // Frequency in Hertz. Set frequency at 4 kHz
+
+#define PWM1 GPIO_NUM_27
+#define FWD1 GPIO_NUM_16
+#define BAK1 GPIO_NUM_17
+#define PWM2 GPIO_NUM_26
+#define FWD2 GPIO_NUM_18
+#define BAK2 GPIO_NUM_19
+
+void init_motor(gpio_num_t pin) {
+  ledc_timer_config_t ledc_timer = {.speed_mode = LEDC_MODE,
+                                    .duty_resolution = LEDC_DUTY_RES,
+                                    .timer_num = LEDC_TIMER,
+                                    .freq_hz = LEDC_FREQUENCY,
+                                    .clk_cfg = LEDC_AUTO_CLK};
+  ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+  ledc_channel_config_t ledc_channel = {.speed_mode = LEDC_MODE,
+                                        .channel = LEDC_CHANNEL,
+                                        .timer_sel = LEDC_TIMER,
+                                        .intr_type = LEDC_INTR_DISABLE,
+                                        .gpio_num = pin,
+                                        .duty = 0,
+                                        .hpoint = 0};
+  ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+}
+
+void app_main(void) {
+  gpio_set_direction(PWM1, GPIO_MODE_OUTPUT);
+  gpio_set_direction(FWD1, GPIO_MODE_OUTPUT);
+  gpio_set_direction(BAK1, GPIO_MODE_OUTPUT);
+
+  gpio_set_direction(PWM2, GPIO_MODE_OUTPUT);
+  gpio_set_direction(FWD2, GPIO_MODE_OUTPUT);
+  gpio_set_direction(BAK2, GPIO_MODE_OUTPUT);
+
+  gpio_set_level(FWD1, 1);
+  gpio_set_level(FWD2, 1);
+
+  init_motor(PWM1);
+  init_motor(PWM2);
+
+  ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+  ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+
+  vTaskDelay(100);
+
+  gpio_set_level(FWD1, 0);
+  gpio_set_level(FWD2, 0);
+
+  vTaskDelay(100);
+
+  gpio_set_level(BAK1, 1);
+  gpio_set_level(BAK2, 1);
+
+  vTaskDelay(100);
+
+  ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, 0));
+  ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+
+  gpio_set_level(BAK1, 0);
+  gpio_set_level(BAK2, 0);
+}
