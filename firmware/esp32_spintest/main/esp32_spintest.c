958119a482e8c0b4c18d48df1a56229a60194608
Joseph <jgoyer3@gmail.com>
2024-12-28 00:47:28 -0500

    update ignore

--- firmware/esp32_spintest/main/esp32_spintest.c
+++ firmware/esp32_spintest/main/esp32_spintest.c
@@ -9,9 +9,9 @@
 #define LEDC_TIMER LEDC_TIMER_0
 #define LEDC_MODE LEDC_LOW_SPEED_MODE
 #define LEDC_CHANNEL LEDC_CHANNEL_0
-#define LEDC_DUTY_RES LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
-#define LEDC_DUTY (4096)      // Set duty to 50%. (2 ** 13) * 50% = 4096
-#define LEDC_FREQUENCY (4000) // Frequency in Hertz. Set frequency at 4 kHz
+#define LEDC_DUTY_RES LEDC_TIMER_13_BIT
+#define LEDC_DUTY (4096)
+#define LEDC_FREQUENCY (4000)
 
 #define PWM1 GPIO_NUM_27
 #define FWD1 GPIO_NUM_16
